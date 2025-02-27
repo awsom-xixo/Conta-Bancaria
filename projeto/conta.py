58aa09937a9b001709d704cd33d56f36376e374d
Micael Junco <micaeljuncoplay4@gmail.com>
2025-02-27 16:52:33 -0300

    Salvo

--- projeto/conta.py
+++ projeto/conta.py
@@ -6,13 +6,13 @@ class ContaCorrente():
     @staticmethod # Comentário que nos ajuda na organizção do código
     def _data_eHora():
         fusoBR = pytz.timezone('Brazil/East')
-        horarioBR = datetime.now(fusoBR)
-        return horarioBR
+        horarioBR = datetime.datetime.now(fusoBR)
+        return horarioBR.strftime('%d/%m/%Y %H:%M:%S') # Formata a data para ficar mais amigável para o usuário
 
 # Parâmetros padrões da classe
 
     def __init__(self,nome,cpf, agencia, num_conta):
-        self.nome = nome
+        self._nome = nome # Atributo privado
         self.cpf = cpf
         self.saldo = 0 # Atributo interno
         self.limite = None # Atributo interno
@@ -32,17 +32,28 @@ class ContaCorrente():
         print('Seu saldo atual é de R${:,.2f}'.format(self.saldo))
         pass
 
-    def sacar_Dinheiro(self,valor): # Valor = parâmetro
-        if (self.saldo < self._limite_daConta()):
-            print('Você não tem saldo suficiente para sacar esse valor. ')
-            self.consultar_Saldo() # Usando um método dentro de outro
+    def consultar_Transacoes(self):
+        print('-' * (len('Histórico de transações: ')))
+        print('Histórico de transações: ')
+        for transacao in self.transacoes:
+            print(transacao)
+
+    def sacar_Dinheiro(self, valor):
+        if (self.saldo - valor < self._limite_daConta()):
+            print('Você não tem saldo suficiente para sacar esse valor.')
+            self.consultar_Saldo()
         else:
             self.saldo -= valor
-            self.registro_deTransacoes(valor)
+            self.registro_deTransacoes(-valor)
             self.consultar_Saldo()
-        pass
 
     def depositar_Dinheiro(self,valor):
         self.saldo += valor
         self.registro_deTransacoes(valor)
         pass
+
+    def transferir(self, valor, conta_destinada):
+        self.saldo -= valor
+        conta_destinada.saldo += valor
+        self.registro_deTransacoes(valor)
+        conta_destinada.registro_deTransacoes(valor)
--- projeto/main.py
+++ projeto/main.py
@@ -1,10 +1,21 @@
 from conta import ContaCorrente
 
-conta_lira = ContaCorrente('Lira','111-222-333-45') 
-conta_lira.consultar_Saldo()
+conta_lira = ContaCorrente('Lira', '111-222-333-45', 4444, 12345)
+conta_maeLira = ContaCorrente('Beth', '222-333-444-56', 5555, 23456)
 
-conta_lira.depositar_Dinheiro(1000)
+conta_lira.consultar_Saldo()
+conta_maeLira.depositar_Dinheiro(100)
+conta_maeLira.consultar_Saldo()
+conta_lira.depositar_Dinheiro(10000)
 
 conta_lira.sacar_Dinheiro(100)
 
+conta_lira.transferir(1000, conta_maeLira)
+conta_maeLira.consultar_Saldo()
+
+
+print('Saldo final:')
+conta_lira.consultar_Saldo()
+
+conta_lira.consultar_Transacoes()
 
